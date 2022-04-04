f8703d55e8a05ec5f619336322aeac153b163f5f
abhishekbali1510 <abhishekbali15oct@gmail.com>
2022-04-04 20:26:24 +0530

    basic validation

--- validation.php
+++ validation.php
@@ -1,6 +1,6 @@
 <?php
 
-$var="Abhishek_bali";
+$var="Abhishek";
 
 // check for empty fieldS
 if (empty($var)) 
@@ -12,16 +12,34 @@ else
         echo $var;  
     }  
 
+    
 //string validation
 if (!preg_match ("/^[a-zA-z_]*$/", $var) ) 
     {
         echo "Only alphabets and underscore are allowed.";
     }
 else
-{  
-    echo $var;  
-}  
+    {  
+        echo $var;  
+    }  
 
 
+//length validation 
+$length = strlen($var);  
+  
+if ($length > 10)
+    {  
+        echo "field must have less than 10 characters.";  
+    }
+ else
+    {    
+        echo $var;  
+    } 
 
+
+//Button click validation
+if (isset ($_POST['submit']))
+    {  
+        echo "Submit button is clicked.";  
+    }
 ?>
